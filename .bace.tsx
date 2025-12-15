5d2c6c2e21b4d67ca71f8a75acc7ff3d12524668
m9020120014 <m9020120014@gmail.com>
2025-12-15 10:52:47 +0330

    Add assets folder - Fonts - Media - svg Edit Css Pelak - Unit - Radius - FontSize

---
+++ .bace.tsx
@@ -0,0 +1,56 @@
+
+/* --- Base ------------------------------------------------------------------------------------- */
+/* --- Lib -------------------------------------------------------------------------------------- */
+/* --- Components ------------------------------------------------------------------------------- */
+/* --- Types ------------------------------------------------------------------------------------ */
+/* --- Data ------------------------------------------------------------------------------------- */
+/* --- Constants -------------------------------------------------------------------------------- */
+/* --- Functions -------------------------------------------------------------------------------- */
+
+// --- // BUG   | need to be done now
+// --- // HACK  | need to be done today
+// --- // DOIT  | need to be done soon
+// --- // TODO  | need to be done later
+// --- // DEBUG | delete this after debugging
+// --- // FIX   | maybe a bug or a problem that needs to be fixed
+// --- // [ ]   | task is not started yet
+// --- // [x]   | task is completed but not tested yet
+// --- // XXX   | ◀︎ | ▶︎ | ▲ | ▼ | 🞬 | 🞸 | ● | ⬡ | ⦿ | ← | → | ↑ | ↓ | ↖︎ | ↗︎ | ↘︎ | ↙︎ | ↔︎ | ↕︎ |
+
+/* --- q.:| ♢ |:.p ------------------------------------------------------------------------------ */
+/* --- q.:| ♢ |:.p -------------------------------------------------- */
+/* --- q.:| ♢ |:.p ------------------ */
+
+// console.log(" --- error | api/auth/[...] | response of fetch ------------------ q.:| ")
+// console.log(" --- alert | api/auth/[...] | response of fetch ------------------ o.:| ")
+// console.log(" --- debug | api/auth/[...] | response of fetch ------------------ |:./ ") // DEBUG < 
+
+// SubmitErrorsClient(
+//   "(auth)/verification",
+//   "handleMobileSubmit TRY",
+//   error instanceof Error ? error : new Error(String(error))
+// );
+// SubmitErrorsStringClient(
+//   "page/[slug]",
+//   "response is not ok",
+//   response.statusText
+// );
+// SubmitErrorsStringServer(
+//   "page/[slug]",
+//   "response is not ok",
+//   response.statusText
+// );
+// SubmitErrorsServer(
+//   "(auth)/verification",
+//   "handleMobileSubmit TRY",
+//   error instanceof Error ? error : new Error(String(error))
+// );
+// SubmitErrorsStringServer(
+//   "page/[slug]",
+//   "response is not ok",
+//   response.statusText
+// );
+// encodeURIComponent(slug)
+
+
+
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,8 +2,25 @@
 
 All notable changes to this project will be documented in this file.
 
-The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
-and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [0.1.5] - 2025-12-14
+
+- add assets folder
+  - Fonts
+  - Media - svg
+- edit Css Pelak
+
+## [0.1.4] - 2025-12-14
+
+- remove globals.css and update import path in layout.tsx
+
+## [0.1.3] - 2025-12-14
+
+- version and push
+
+## [0.1.2] - 2025-12-14
+
+- tag and version and push
 
 ## [0.1.1] - 2025-12-14
 
